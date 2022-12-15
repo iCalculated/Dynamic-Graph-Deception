a55c6f9c7be29ba5ec288d223ea8cbcbdbd0b332
iCalculated <fufa0001@gmail.com>
2022-12-14 19:33:02 -0600

    message function

--- tgn/modules/message_function.py
+++ tgn/modules/message_function.py
@@ -2,39 +2,57 @@ from torch import nn
 
 
 class MessageFunction(nn.Module):
-  """
-  Module which computes the message for a given interaction.
-  """
+    """
+    Module which computes the message for a given interaction.
+    """
 
-  def compute_message(self, raw_messages):
-    return None
+    def compute_message(self, raw_messages):
+        return None
 
 
 class MLPMessageFunction(MessageFunction):
-  def __init__(self, raw_message_dimension, message_dimension):
-    super(MLPMessageFunction, self).__init__()
+    """
+    MLP message function to calculate the message m(t)
 
-    self.mlp = self.layers = nn.Sequential(
-      nn.Linear(raw_message_dimension, raw_message_dimension // 2),
-      nn.ReLU(),
-      nn.Linear(raw_message_dimension // 2, message_dimension),
-    )
+    INPUT:
+        raw_message_dimension: Dimension of the raw_message
+        message_dimension: Dimension of the message
+        raw_messages: [S_i(t-1) || S_j(t-1) || delta_t || e(t)] for interaction events
 
-  def compute_message(self, raw_messages):
-    messages = self.mlp(raw_messages)
+    OUTPUT:
+        message: m(t) <-- [S_i(t-1) || S_j(t-1) || delta_t || e(t)] for interation events
 
-    return messages
+    """
+
+    def __init__(self, raw_message_dimension, message_dimension):
+        super(MLPMessageFunction, self).__init__()
+
+        self.mlp = self.layers = nn.Sequential(
+            nn.Linear(raw_message_dimension, raw_message_dimension // 2),
+            nn.ReLU(),
+            nn.Linear(raw_message_dimension // 2, message_dimension),
+        )
+
+    def compute_message(self, raw_messages):
+        messages = self.mlp(raw_messages)
+
+        return messages
 
 
 class IdentityMessageFunction(MessageFunction):
+    """
+    message function returns m(t) = raw_message
+    """
 
-  def compute_message(self, raw_messages):
+    def compute_message(self, raw_messages):
 
-    return raw_messages
+        return raw_messages
 
 
 def get_message_function(module_type, raw_message_dimension, message_dimension):
-  if module_type == "mlp":
-    return MLPMessageFunction(raw_message_dimension, message_dimension)
-  elif module_type == "identity":
-    return IdentityMessageFunction()
+    if module_type == "mlp":
+        return MLPMessageFunction(raw_message_dimension, message_dimension)
+    elif module_type == "identity":
+        return IdentityMessageFunction()
+    else:
+        raise ValueError("Message function {} not implemented".format(module_type))
