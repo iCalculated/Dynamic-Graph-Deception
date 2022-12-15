eaa632f319379d75e4c6b42b5e4cc84e6acc69b5
iCalculated <fufa0001@gmail.com>
2022-12-14 19:30:41 -0600

    memory_update module

--- tgn/modules/memory_updater.py
+++ tgn/modules/memory_updater.py
@@ -3,66 +3,128 @@ import torch
 
 
 class MemoryUpdater(nn.Module):
-  def update_memory(self, unique_node_ids, unique_messages, timestamps):
-    pass
+    def update_memory(self, unique_node_ids, unique_messages, timestamps):
+        pass
 
 
 class SequenceMemoryUpdater(MemoryUpdater):
-  def __init__(self, memory, message_dimension, memory_dimension, device):
-    super(SequenceMemoryUpdater, self).__init__()
-    self.memory = memory
-    self.layer_norm = torch.nn.LayerNorm(memory_dimension)
-    self.message_dimension = message_dimension
-    self.device = device
+    """
+    RNN based memory updater.
+    Node's memory as the hidden state of RNN, aggregated message as the input, updated memory as the new hidden state
 
-  def update_memory(self, unique_node_ids, unique_messages, timestamps):
-    if len(unique_node_ids) <= 0:
-      return
+    INPUT:
+        memory: Memory instance
+        message_dimension: The dim of the message
+        memory_dimension: The dim of the memory
+        ---------------------------------------
+        unique_node_ids: A list of unique node ids
+        unique_messages: A tensor of shape [unique_node_ids, aggregated message]
+        timestamps: A tensor contains corresponding timestamp for those aggregated messages
 
-    assert (self.memory.get_last_update(unique_node_ids) <= timestamps).all().item(), "Trying to " \
-                                                                                     "update memory to time in the past"
+    OUTPUT:
+        update_memory(): There is no output, we update node's memory by referring set_memory() method
 
-    memory = self.memory.get_memory(unique_node_ids)
-    self.memory.last_update[unique_node_ids] = timestamps
+        updated_memory: A tensor of shape [unique_nodes, memory_dimension]
+        updated_last_update: A tensor of shape [unique_nodes]
 
-    updated_memory = self.memory_updater(unique_messages, memory)
+    EXAMPLE (last_aggregator + memory_updator):
+            node_ids = [0, 1, 1, 2]
+            messages = {0: [(tensor([1., 2., 3., 4., 5.]), tensor(1))],
+                        1: [(tensor([2., 3., 4., 5., 6.]), tensor(1)), (tensor([3., 4., 5., 6., 7.]), tensor(2))],
+                        2: [(tensor([4., 5., 6., 7., 8.]), tensor(2))]}
 
-    self.memory.set_memory(unique_node_ids, updated_memory)
+        ==>
+            to_update_node_ids: [0, 1, 2]
 
-  def get_updated_memory(self, unique_node_ids, unique_messages, timestamps):
-    if len(unique_node_ids) <= 0:
-      return self.memory.memory.data.clone(), self.memory.last_update.data.clone()
+            unique_messages: tensor([[1., 2., 3., 4., 5.],
+                                      [3., 4., 5., 6., 7.],
+                                      [4., 5., 6., 7., 8.]])
 
-    assert (self.memory.get_last_update(unique_node_ids) <= timestamps).all().item(), "Trying to " \
-                                                                                     "update memory to time in the past"
+            unique_timestamps: tensor([1., 2., 2.])
 
-    updated_memory = self.memory.memory.data.clone()
-    updated_memory[unique_node_ids] = self.memory_updater(unique_messages, updated_memory[unique_node_ids])
+            ==>
 
-    updated_last_update = self.memory.last_update.data.clone()
-    updated_last_update[unique_node_ids] = timestamps
+                updated_memory: tensor([[-0.8096],
+                                        [-0.9309],
+                                        [-0.9771]], grad_fn=<IndexPutBackward>)
 
-    return updated_memory, updated_last_update
+                updated_last_update: tensor([1., 2., 2.])
+    """
+
+    def __init__(self, memory, message_dimension, memory_dimension, device):
+        super(SequenceMemoryUpdater, self).__init__()
+        self.memory = memory
+        self.layer_norm = torch.nn.LayerNorm(memory_dimension)
+        self.message_dimension = message_dimension
+        self.device = device
+
+    def update_memory(self, unique_node_ids, unique_messages, timestamps):
+        """
+        Linked with Memory instance to update node's memory
+        """
+        if len(unique_node_ids) <= 0:
+            return
+
+        assert (
+            (self.memory.get_last_update(unique_node_ids) <= timestamps).all().item()
+        ), ("Trying to " "update memory to time in the past")
+
+        memory = self.memory.get_memory(unique_node_ids)
+        self.memory.last_update[unique_node_ids] = timestamps
+
+        # E.g. S_1(t1) <-- aggregated message m^line_1(t1) + previous memory S_1(t0)
+        updated_memory = self.memory_updater(unique_messages, memory)
+
+        self.memory.set_memory(unique_node_ids, updated_memory)
+
+    def get_updated_memory(self, unique_node_ids, unique_messages, timestamps):
+        """
+        Detached from Memory instance to update node's memory and then return updated_memory and updated_last_update
+        """
+        if len(unique_node_ids) <= 0:
+            return self.memory.memory.data.clone(), self.memory.last_update.data.clone()
+
+        assert (
+            (self.memory.get_last_update(unique_node_ids) <= timestamps).all().item()
+        ), ("Trying to " "update memory to time in the past")
+
+        updated_memory = self.memory.memory.data.clone()
+        updated_memory[unique_node_ids] = self.memory_updater(
+            unique_messages, updated_memory[unique_node_ids]
+        )
+
+        updated_last_update = self.memory.last_update.data.clone()
+        updated_last_update[unique_node_ids] = timestamps
+
+        return updated_memory, updated_last_update
 
 
 class GRUMemoryUpdater(SequenceMemoryUpdater):
-  def __init__(self, memory, message_dimension, memory_dimension, device):
-    super(GRUMemoryUpdater, self).__init__(memory, message_dimension, memory_dimension, device)
+    def __init__(self, memory, message_dimension, memory_dimension, device):
+        super(GRUMemoryUpdater, self).__init__(
+            memory, message_dimension, memory_dimension, device
+        )
 
-    self.memory_updater = nn.GRUCell(input_size=message_dimension,
-                                     hidden_size=memory_dimension)
+        self.memory_updater = nn.GRUCell(
+            input_size=message_dimension, hidden_size=memory_dimension
+        )
 
 
 class RNNMemoryUpdater(SequenceMemoryUpdater):
-  def __init__(self, memory, message_dimension, memory_dimension, device):
-    super(RNNMemoryUpdater, self).__init__(memory, message_dimension, memory_dimension, device)
+    def __init__(self, memory, message_dimension, memory_dimension, device):
+        super(RNNMemoryUpdater, self).__init__(
+            memory, message_dimension, memory_dimension, device
+        )
 
-    self.memory_updater = nn.RNNCell(input_size=message_dimension,
-                                     hidden_size=memory_dimension)
+        self.memory_updater = nn.RNNCell(
+            input_size=message_dimension, hidden_size=memory_dimension
+        )
 
 
-def get_memory_updater(module_type, memory, message_dimension, memory_dimension, device):
-  if module_type == "gru":
-    return GRUMemoryUpdater(memory, message_dimension, memory_dimension, device)
-  elif module_type == "rnn":
-    return RNNMemoryUpdater(memory, message_dimension, memory_dimension, device)
+def get_memory_updater(
+    module_type, memory, message_dimension, memory_dimension, device
+):
+    if module_type == "gru":
+        return GRUMemoryUpdater(memory, message_dimension, memory_dimension, device)
+    elif module_type == "rnn":
+        return RNNMemoryUpdater(memory, message_dimension, memory_dimension, device)
